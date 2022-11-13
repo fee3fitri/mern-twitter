5f3ad2485092a8af9f4de7c586eb717b00fa4bf9
fee3fitri <safitri.shelton@gmail.com>
2022-11-12 23:56:53 -0800

    Create api/tweets router

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/users'); // update the import file path
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users'); // update the import file path
+const tweetsRouter = require('./routes/api/tweets');
+const app = express();
 
 app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // serve the static fil
 
 // Attach Express routers
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter); // update the path
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;
