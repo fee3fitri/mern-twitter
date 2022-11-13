dba7c3ad184db91986e7b2e7207939baab753d31
fee3fitri <safitri.shelton@gmail.com>
2022-11-12 23:52:48 -0800

    Create api/users router

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/users'); // update the import file path
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
 
+// Attach Express routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter); // update the path
 
 module.exports = app;
