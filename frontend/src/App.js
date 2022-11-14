74a2dfa5549d0892585eeea60d2a4216289dd5d3
fee3fitri <safitri.shelton@gmail.com>
2022-11-13 22:53:32 -0800

    Finished frontend auth phase 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Switch } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
+
 import MainPage from './components/MainPage/MainPage';
-import LoginForm from './components/SessionForms/LoginForm';
-import SignupForm from './components/SessionForms/SignupForm';
+import LoginForm from './components/SessionForms/LoginForm.js';
+import SignupForm from './components/SessionForms/SignupForm.js';
+
+import { getCurrentUser } from './store/session';
 
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
+  return loaded && (
     <>
       <NavBar />
       <Switch>
